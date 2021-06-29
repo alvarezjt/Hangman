66726374acaab6e0c9a4086a7c3b6c3e9bce7ce2
Juan Thomas Alvarez <alvarezjt@hotmail.com>
2021-06-29 17:53:40 -0400

    Created colors in tuples, and filled the game background with white.

--- Hangman/HamgmanTheNewBostonPygame.py
+++ Hangman/HamgmanTheNewBostonPygame.py
@@ -6,6 +6,13 @@ import pygame as pygame
 import pygame.display as pygamedisplay
 import pygame.event as pygameevent
 
+#Color Constants
+colorBlack = (0, 0, 0)
+colorWhite = (255, 255, 255)
+colorRed = (254, 0, 0)
+colorGreen = (0, 254, 0)
+colorBlue = (0, 0, 254)
+
 pygame.init()
 gameDisplay = pygamedisplay.set_mode((800,600))
 pygamedisplay.update() # pygamedisplay.flip() can be used alternatively
@@ -16,5 +23,9 @@ RunGame = True
 while RunGame:
 	for event in pygameevent.get():
 		print(event)
+		if event.type == pygame.QUIT:
+			RunGame = False
+	gameDisplay.fill(colorWhite)
+	pygamedisplay.update()
 pygame.quit()
 quit
